daa260dc1e09fc381417c38cdfb6fe9e40b6f58c
xiaofan <1353815633@qq.com>
2018-05-08 18:10:50 +0800

    part one

--- school-attendance/js/app.js
+++ school-attendance/js/app.js
@@ -1,84 +1,183 @@
+/* Model */
+var model = {
+    tdTitle: {
+        title: 'Student Name',
+        days: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+        missedCol: 'Days Missed-col'
+    },
+
+    student: [
+        {
+            name: 'Slappy the Frog',
+            attend: [true, false, true, false, true, false, true, false, true, false, true, false],
+            missed: 0
+        },
+        {
+            name: 'Lilly the Lizard',
+            attend: [true, true, false, false, true, false, true, false, true, false, true, false],
+            missed: 0
+        },
+        {
+            name: 'Paulrus the Walrus',
+            attend: [false, false, true, false, true, false, true, false, true, false, true, false],
+            missed: 0
+        },
+        {
+            name: 'Gregory the Goat',
+            attend: [false, true, false, true, false, true, false, true, false, true, false, true],
+            missed: 0
+        },
+        {
+            name: 'Adam the Anaconda',
+            attend: [true, false, false, true, false, true, false, true, false, true, false, false],
+            missed: 0
+        }
+    ]
+};
+
+
+/* View */
+
+
+
+/* octopus */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /* STUDENTS IGNORE THIS FUNCTION
  * All this does is create an initial
  * attendance record if one is not found
  * within localStorage.
  */
-(function() {
-    if (!localStorage.attendance) {
-        console.log('Creating attendance records...');
-        function getRandom() {
-            return (Math.random() >= 0.5);
-        }
+// (function() {
+//     if (!localStorage.attendance) {
+//         console.log('Creating attendance records...');
+//         function getRandom() {
+//             return (Math.random() >= 0.5);
+//         }
 
-        var nameColumns = $('tbody .name-col'),
-            attendance = {};
+//         var nameColumns = $('tbody .name-col'),
+//             attendance = {};
 
-        nameColumns.each(function() {
-            var name = this.innerText;
-            attendance[name] = [];
+//         nameColumns.each(function() {
+//             var name = this.innerText;
+//             attendance[name] = [];
 
-            for (var i = 0; i <= 11; i++) {
-                attendance[name].push(getRandom());
-            }
-        });
+//             for (var i = 0; i <= 11; i++) {
+//                 attendance[name].push(getRandom());
+//             }
+//         });
 
-        localStorage.attendance = JSON.stringify(attendance);
-    }
-}());
+//         localStorage.attendance = JSON.stringify(attendance);
+//     }
+// }());
 
 
-/* STUDENT APPLICATION */
-$(function() {
-    var attendance = JSON.parse(localStorage.attendance),
-        $allMissed = $('tbody .missed-col'),
-        $allCheckboxes = $('tbody input');
+// /* STUDENT APPLICATION */
+// $(function() {
+//     var attendance = JSON.parse(localStorage.attendance),
+//         $allMissed = $('tbody .missed-col'),
+//         $allCheckboxes = $('tbody input');
 
-    // Count a student's missed days
-    function countMissing() {
-        $allMissed.each(function() {
-            var studentRow = $(this).parent('tr'),
-                dayChecks = $(studentRow).children('td').children('input'),
-                numMissed = 0;
+//     // Count a student's missed days
+//     function countMissing() {
+//         $allMissed.each(function() {
+//             var studentRow = $(this).parent('tr'),
+//                 dayChecks = $(studentRow).children('td').children('input'),
+//                 numMissed = 0;
 
-            dayChecks.each(function() {
-                if (!$(this).prop('checked')) {
-                    numMissed++;
-                }
-            });
+//             dayChecks.each(function() {
+//                 if (!$(this).prop('checked')) {
+//                     numMissed++;
+//                 }
+//             });
 
-            $(this).text(numMissed);
-        });
-    }
+//             $(this).text(numMissed);
+//         });
+//     }
 
-    // Check boxes, based on attendace records
-    $.each(attendance, function(name, days) {
-        var studentRow = $('tbody .name-col:contains("' + name + '")').parent('tr'),
-            dayChecks = $(studentRow).children('.attend-col').children('input');
+//     // Check boxes, based on attendace records
+//     $.each(attendance, function(name, days) {
+//         var studentRow = $('tbody .name-col:contains("' + name + '")').parent('tr'),
+//             dayChecks = $(studentRow).children('.attend-col').children('input');
 
-        dayChecks.each(function(i) {
-            $(this).prop('checked', days[i]);
-        });
-    });
+//         dayChecks.each(function(i) {
+//             $(this).prop('checked', days[i]);
+//         });
+//     });
 
-    // When a checkbox is clicked, update localStorage
-    $allCheckboxes.on('click', function() {
-        var studentRows = $('tbody .student'),
-            newAttendance = {};
+//     // When a checkbox is clicked, update localStorage
+//     $allCheckboxes.on('click', function() {
+//         var studentRows = $('tbody .student'),
+//             newAttendance = {};
 
-        studentRows.each(function() {
-            var name = $(this).children('.name-col').text(),
-                $allCheckboxes = $(this).children('td').children('input');
+//         studentRows.each(function() {
+//             var name = $(this).children('.name-col').text(),
+//                 $allCheckboxes = $(this).children('td').children('input');
 
-            newAttendance[name] = [];
+//             newAttendance[name] = [];
 
-            $allCheckboxes.each(function() {
-                newAttendance[name].push($(this).prop('checked'));
-            });
-        });
+//             $allCheckboxes.each(function() {
+//                 newAttendance[name].push($(this).prop('checked'));
+//             });
+//         });
 
-        countMissing();
-        localStorage.attendance = JSON.stringify(newAttendance);
-    });
+//         countMissing();
+//         localStorage.attendance = JSON.stringify(newAttendance);
+//     });
 
-    countMissing();
-}());
+//     countMissing();
+// }());
